7d80345622c3bb05aa236c4d5168a53bf7da1f0f
bm8834 <meisenbacher.stefan@web.de>
2023-08-11 11:08:44 +0200

    add gamma 'scale' and 'auto' option

--- model/MSVR.py
+++ model/MSVR.py
@@ -14,10 +14,12 @@ Multi-output Support Vector Regression
 
 
 import numpy as np
+import scipy.sparse as sp
 from sklearn.metrics.pairwise import pairwise_kernels
 
+
 class MSVR():
-    def __init__(self, kernel='rbf', degree=3, gamma=None, coef0=0.0, tol=0.001, C=1.0, epsilon=0.1):
+    def __init__(self, kernel='rbf', degree=3, gamma="scale", coef0=0.0, tol=0.001, C=1.0, epsilon=0.1):
         super(MSVR, self).__init__()
         self.kernel = kernel
         self.degree = degree
@@ -26,28 +28,37 @@ class MSVR():
         self.tol = tol
         self.C = C
         self.epsilon = epsilon
-        self.Beta = None
-        self.NSV = None
-        self.xTrain = None
 
-    def fit(self, x, y):
-        self.xTrain = x.copy()
+        self._Beta = None
+        self._NSV = None
+        self._X_train = None
+
+    def fit(self, X, y):
+        self._X_train = X.copy()
         C = self.C
         epsi = self.epsilon
         tol = self.tol
 
-        n_m = np.shape(x)[0]  # num of samples
-        n_d = np.shape(x)[1]  # input data dimensionality
+        n_m = np.shape(X)[0]  # num of samples
+        n_d = np.shape(X)[1]  # input data dimensionality
         n_k = np.shape(y)[1]  # output data dimensionality (output variables)
 
-        # H = kernelmatrix(ker, x, x, par)
-        H = pairwise_kernels(x, x, metric=self.kernel, filter_params=True,
-                             degree=self.degree, gamma=self.gamma, coef0=self.coef0)
+        if self.gamma == "scale":
+            X_var = (X.multiply(X)).mean() - (X.mean()) ** 2 if sp.isspmatrix(X) else X.var()
+            self._gamma = 1.0 / (n_d * X_var) if X_var != 0 else 1.0
+        elif self.gamma == "auto":
+            self._gamma = 1.0 / n_d
+        else:
+            self._gamma = self.gamma
 
-        self.Beta = np.zeros((n_m, n_k))
+        # H = kernelmatrix(ker, x, x, par)
+        H = pairwise_kernels(X, X, metric=self.kernel, filter_params=True,
+                             degree=self.degree, gamma=self._gamma, coef0=self.coef0)
+
+        self._Beta = np.zeros((n_m, n_k))
 
         #E = prediction error per output (n_m * n_k)
-        E = y - np.dot(H, self.Beta)
+        E = y - np.dot(H, self._Beta)
         #RSE
         u = np.sqrt(np.sum(E**2, 1, keepdims=True))
 
@@ -70,7 +81,7 @@ class MSVR():
 
         #Lp is the quantity to minimize (sq norm of parameters + slacks)
         Lp = []
-        BetaH = np.dot(np.dot(self.Beta.T, H), self.Beta)
+        BetaH = np.dot(np.dot(self._Beta.T, H), self._Beta)
         Lp_0 = np.sum(np.diag(BetaH), 0) / 2 + C * np.sum(L)/2
         Lp.append(Lp_0)
 
@@ -80,7 +91,7 @@ class MSVR():
         val = 1
 
         while(hacer):
-            Beta_a = self.Beta.copy()
+            Beta_a = self._Beta.copy()
             E_a = E.copy()
             u_a = u.copy()
             i1_a = i1.copy()
@@ -89,15 +100,14 @@ class MSVR():
                 np.diagflat(1/a[i1]) + 1e-10 * np.eye(len(a[i1]))
 
             #compute betas
-            #       sal1 = np.dot(np.linalg.pinv(M1),y[i1])  #求逆or广义逆（M-P逆）无法保证M1一定是可逆的？
             sal1 = np.dot(np.linalg.inv(M1), y[i1])
 
             eta = 1
-            self.Beta = np.zeros(self.Beta.shape)
-            self.Beta[i1] = sal1.copy()
+            self._Beta = np.zeros(self._Beta.shape)
+            self._Beta[i1] = sal1.copy()
 
             #error
-            E = y - np.dot(H, self.Beta)
+            E = y - np.dot(H, self._Beta)
             #RSE
             u = np.sqrt(np.sum(E**2, 1)).reshape(n_m, 1)
             i1 = np.where(u >= epsi)[0]
@@ -106,7 +116,7 @@ class MSVR():
             L[i1] = u[i1]**2 - 2 * epsi * u[i1] + epsi**2
 
             #%recompute the loss function
-            BetaH = np.dot(np.dot(self.Beta.T, H), self.Beta)
+            BetaH = np.dot(np.dot(self._Beta.T, H), self._Beta)
             Lp_k = np.sum(np.diag(BetaH), 0) / 2 + C * np.sum(L)/2
             Lp.append(Lp_k)
 
@@ -115,26 +125,26 @@ class MSVR():
                 eta = eta/10
                 i1 = i1_a.copy()
 
-                self.Beta = np.zeros(self.Beta.shape)
+                self._Beta = np.zeros(self._Beta.shape)
                 #%the new betas are a combination of the current (sal1)
                 #and of the previous iteration (Beta_a)
-                self.Beta[i1] = eta*sal1 + (1-eta)*Beta_a[i1]
+                self._Beta[i1] = eta * sal1 + (1 - eta) * Beta_a[i1]
 
-                E = y - np.dot(H, self.Beta)
+                E = y - np.dot(H, self._Beta)
                 u = np.sqrt(np.sum(E**2, 1)).reshape(n_m, 1)
 
                 i1 = np.where(u >= epsi)[0]
 
                 L = np.zeros(u.shape)
                 L[i1] = u[i1]**2 - 2 * epsi * u[i1] + epsi**2
-                BetaH = np.dot(np.dot(self.Beta.T, H), self.Beta)
+                BetaH = np.dot(np.dot(self._Beta.T, H), self._Beta)
                 Lp_k = np.sum(np.diag(BetaH), 0) / 2 + C * np.sum(L)/2
                 Lp[k] = Lp_k
 
                 #stopping criterion 1
                 if(eta < 1e-16):
                     Lp[k] = Lp[k-1] - 1e-15
-                    self.Beta = Beta_a.copy()
+                    self._Beta = Beta_a.copy()
 
                     u = u_a.copy()
                     i1 = i1_a.copy()
@@ -156,15 +166,13 @@ class MSVR():
             #stopping criterion #algorithm does not converge. (val = -1)
             if(len(i1) == 0):
                 hacer = 0
-                self.Beta = np.zeros(self.Beta.shape)
+                self._Beta = np.zeros(self._Beta.shape)
                 val = -1
 
-        self.NSV = len(i1)
+        self._NSV = len(i1)
 
-    def predict(self, x):
-        H = pairwise_kernels(x, self.xTrain, metric=self.kernel, filter_params=True,
-                             degree=self.degree, gamma=self.gamma, coef0=self.coef0)
-        yPred = np.dot(H, self.Beta)
-        return yPred
-
-    # def score(self,x):
+    def predict(self, X):
+        H = pairwise_kernels(X, self._X_train, metric=self.kernel, filter_params=True,
+                             degree=self.degree, gamma=self._gamma, coef0=self.coef0)
+        y_pred = np.dot(H, self._Beta)
+        return y_pred
